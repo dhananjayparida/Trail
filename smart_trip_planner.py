e393aa2a54fb4ed7ce9fe2dc2185ece6fddbc1fc
dhananjayparida <dhananjayparida149@gmail.com>
2025-11-07 00:02:27 +0530

    Update smart_trip_planner.py

--- smart_trip_planner.py
+++ smart_trip_planner.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-from flask_restful import Api, Resource
-import aitrip as at
-
+from flask import Flask
 app = Flask(__name__)
-
-api = Api(app)
 
 @app.route('/', methods=['GET'])
 def home(): 
-    return "hello"
-
-@app.route('/Smart_Trip_Planner/<string:source>/<string:destination>/<int:days>/<int:budget>', methods=['GET'])
-def smart_trip_planner(source , destination, days, budget):
-    detail=at.generate_trip_plan(source, destination, days, budget).replace("\n", "<br>")
-    return jsonify(detail)
-
-
+    return "hello this is my Fist deployed program"
+# @app.route('/Smart_Trip_Planner/<string:source>/<string:destination>/<int:days>/<int:budget>', methods=['GET'])
+# def smart_trip_planner(source , destination, days, budget):
+#     detail=at.generate_trip_plan(source, destination, days, budget).replace("\n", "<br>")
+#     return jsonify(detail)
 app.run(debug=True)
