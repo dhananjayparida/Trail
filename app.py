0cf49073951484f401e425c4ff3bbc4cbe7d6426
dhananjayparida <dhananjayparida149@gmail.com>
2025-11-07 00:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def home():
 # def smart_trip_planner(source , destination, days, budget):
 #     detail=at.generate_trip_plan(source, destination, days, budget).replace("\n", "<br>")
 #     return jsonify(detail)
-app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
